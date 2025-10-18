19926484e5f66dc0f07e8d9720497aab8e8bc394
kgrant746 <kgrant746@gmail.com>
2025-10-17 22:15:25 -0500

    feat: added seasonal profile picture changer function

---
+++ task_manager.py
@@ -0,0 +1,64 @@
+from discord.ext import commands, tasks
+from datetime import datetime, date, time
+
+bot: commands.Bot | None = None
+LOCAL_TZ = datetime.now().astimezone().tzinfo
+
+
+#------------------ Holiday Profile Picture Changer ------------------#
+
+TRIGGER_DATES = [
+    (10, 17), 
+    (11, 1), 
+    (1, 1)
+]
+
+CHRISTMAS_PIC = "C:\\Users\\Recor\\Kirbo\\bin\\profile-pictures\\kirbo-christmas.png"
+HALLOWEEN_PIC = "C:\\Users\\Recor\\Kirbo\\bin\\profile-pictures\\kirbo-halloween.png"
+
+@tasks.loop(time=time(0, 0, tzinfo=LOCAL_TZ))
+async def holiday_check():
+    await run_holiday_check()
+
+async def run_holiday_check():
+    today = datetime.now().date()
+
+    print(f"Today is {today}")
+    if (today.month, today.day) in TRIGGER_DATES:
+        channel = await bot.fetch_channel(530799669597700147) # currently BOT TEST channel
+        print("Trigger date matched!")
+
+        if today.month == 11:
+            print("It's November, setting Christmas pic")
+            await holiday_time(CHRISTMAS_PIC)
+            await channel.send("JSCHLATT TIME!")
+        elif today.month == 10:
+            print("It's October, setting Halloween pic")
+            await holiday_time(HALLOWEEN_PIC)
+            await channel.send("SPOOKY TIME!")
+
+async def holiday_time(image_path: str):
+    with open(image_path, "rb") as img:
+        await bot.user.edit(avatar=img.read())
+
+
+#------------------ COD Countdown Channel Name Updater ------------------#
+CHANNEL_ID = 1428195301188960286  # voice channel ID
+TARGET_DATE = date(2025, 11, 14)  # set the CoD release date (YYYY, M, D)
+
+def make_name(days: int) -> str:
+    if days <= 0:
+        return "CoD is out"
+    unit = "DAY" if days == 1 else "DAYS"
+    return f"{days} {unit} TILL COD"
+
+async def set_channel_name():
+    now = datetime.now().date()  # system local date
+    days_left = (TARGET_DATE - now).days
+    name = make_name(days_left)
+    channel = await bot.fetch_channel(CHANNEL_ID)
+    await channel.edit(name=name)
+
+@tasks.loop(time=time(0, 0, tzinfo=LOCAL_TZ))
+async def nightly_update():
+    await set_channel_name()
